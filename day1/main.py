b4e1e2f3c0ce3599a85e5b012df87d72508ae937
Nathan Hitchcock <25087626+nathanhitchcock@users.noreply.github.com>
2021-12-14 16:41:32 -0600

    bug fix

--- day1/main.py
+++ day1/main.py
@@ -1,28 +1,17 @@
 # puzzle from https://adventofcode.com/2021/day/1
 
-'''
 #input the numbers from the dataset
---------------------------------------------------------------
-'''
-file = open("input.dat")
-lst=[]
+#--------------------------------------------------------------
+lst = open("input.dat").readlines()
 
-#Extract all integers from the file into a list
-def extract_to_list(file, lst):
-    for line in file:
-        lst.append(line.strip())
-    return(lst)
 
-extract_to_list(file,lst)
+#for each value in the list(index), compare it to the next value (index + 1)
+#--------------------------------------------------------------
 
-'''
-for each value in the list(index), compare it to the next value (index + 1)
---------------------------------------------------------------
-'''
 count = 0
 for index, current_num in enumerate(lst[:-1]):
-    next_num = lst[index + 1]
-    if current_num < next_num:
+    next_num = int(lst[index + 1])
+    if int(current_num) < next_num:
         count += 1
         print(str(index) + ' ' + str(current_num) + ' ' + str(next_num) + ' ' + 'count ' +str(count))
     else:
