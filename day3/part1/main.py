16598cd9325dcec8b64c81e23da05034c9e4dd06
Nathan Hitchcock <nathan.hitchcock@outlook.com>
2021-12-16 16:48:42 -0600

    WIP

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -12,6 +12,8 @@ epsilon_rate = 0
 #The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
 power_consumption = gamma_rate * epsilon_rate
 
+count_bit0 = 0
+count_bit1 = 0
 
 #input the numbers from the dataset
 #--------------------------------------------------------------
@@ -19,6 +21,16 @@ lst = open("input.dat").readlines()
 
 arr = np.array(lst)
 
-for x in arr[1]:
-    for y in x:
-        print(y)
+#testing pulling the array elements
+for x in arr:
+    if x[0] == "0":
+        count_bit0 += 1
+    else:
+        count_bit1 += 1
+
+if count_bit0 > count_bit1:
+    print("the first bit is 0")
+else:
+    print("the first bit is 1")
+
+print (count_bit0, count_bit1)
