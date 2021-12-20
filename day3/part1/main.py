cabb9c01c1e1c8f2b58c3f38b7c401237332f79c
Nathan Hitchcock <25087626+nathanhitchcock@users.noreply.github.com>
2021-12-19 21:04:48 -0600

    WIP

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -4,10 +4,10 @@ import numpy as np
 #delcaring the variables
 #--------------------------------------------------------------
 #gamma rate is most common bit in each position
-gamma_rate = 0
+gamma_rate = []
 
 #epsilon_rate is inverse of the gamma bit (least common)
-epsilon_rate = 0
+epsilon_rate = []
 
 #The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
 power_consumption = gamma_rate * epsilon_rate
@@ -22,22 +22,28 @@ lst = open("input.dat").readlines()
 arr = np.array(lst)
 
 #testing pulling the array elements
-for x in arr:
-    if x[0] == "0":
+def most_frequent(i, gamma_rate, epsilon_rate):
+    count_bit0 = 0
+    count_bit1 = 0
+    #print(type(i), i)
+    if int(i) == 0:
         count_bit0 += 1
     else:
         count_bit1 += 1
-if count_bit0 > count_bit1:
-    print("the first bit is 0")
-else:
-    print("the first bit is 1")
+    if count_bit0 > count_bit1:
+        gamma_rate.append(i)
+        return gamma_rate
+    else:
+        epsilon_rate.append(i)
+        return epsilon_rate
 
-for x in arr:
-    if x[1] == "0":
-        count_bit0 += 1
-    else:
-        count_bit1 += 1
-if count_bit0 > count_bit1:
-    print("the second bit is 0")
-else:
-    print("the second bit is 1")
+def main():
+    for col in zip(*arr):
+        for i in col:
+            print(i)
+            most_frequent(i, gamma_rate, epsilon_rate)
+    print('the gamma_rate is: ', gamma_rate)
+    print('the epsilon_rate is: ', epsilon_rate)
+
+if __name__ == "__main__":
+    main()
