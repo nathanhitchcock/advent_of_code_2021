3884f1452e087260272db79162677a51b1097637
Nathan Hitchcock <25087626+nathanhitchcock@users.noreply.github.com>
2021-12-20 14:23:16 -0600

    part 1 solved!

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -19,7 +19,7 @@ lst = open("input.dat").readlines()
 
 arr = np.array(lst)
 
-def main():
+def find_gamma(arr,gamma_rate):
     #iterate through each column of the dataset
     for col in zip(*arr):
         #identify the mode (most common) character in the column
@@ -28,6 +28,7 @@ def main():
         #add the mode to the gamma_rate list
         gamma_rate.append(most_frequent)
 
+def find_epsilon(gamma_rate, epsilon_rate):
     #once we have the gamma_rate values, the epsilon_rate is the inverse:
     for i in gamma_rate:
         if i == "0":
@@ -36,6 +37,26 @@ def main():
             epsilon_rate.append("0")
     print('the gamma_rate is: ', gamma_rate)
     print('the epsilon_rate is: ', epsilon_rate)
+
+def convert_to_decimal(gamma_rate, epsilon_rate):
+    #convert the incomming list to a binary number
+    gamma_binary = bin(int(''.join(map(str, gamma_rate)), 2) << 1)
+    epsilon_binary = bin(int(''.join(map(str, epsilon_rate)), 2) << 1)
+
+    #convert the binary number to a decimal
+    #I don't know why I have to % by 2 here...
+    gamma_decimal = (int(gamma_binary, 2)) / 2
+    print('the gamma_decimal is: ', gamma_decimal)
+    epsilon_decimal = (int(epsilon_binary, 2)) / 2
+    print('the epsilon_decimal is: ', epsilon_decimal)
+
+    power_consumption = gamma_decimal * epsilon_decimal
+    print('the power consumption is: ', power_consumption)
+
+def main():
+    find_gamma(arr,gamma_rate)
+    find_epsilon(gamma_rate, epsilon_rate)
+    convert_to_decimal(gamma_rate, epsilon_rate)
 
 if __name__ == "__main__":
     main()
