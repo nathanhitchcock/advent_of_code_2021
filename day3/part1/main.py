513bb95acf8eac2806b44aee5886b2c8f41bd3f6
Nathan Hitchcock <nathan.hitchcock@outlook.com>
2021-12-19 22:45:57 -0600

    mode update
    changed the logic to include mode instead of counters, way easier!

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import statistics as st
 
 #delcaring the variables
 #--------------------------------------------------------------
@@ -9,39 +10,30 @@ gamma_rate = []
 #epsilon_rate is inverse of the gamma bit (least common)
 epsilon_rate = []
 
-#The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
-power_consumption = gamma_rate * epsilon_rate
-
-count_bit0 = 0
-count_bit1 = 0
+#The power consumption can then be found by multiplying the gamma rate by the epsilon rate (decimal format).
+#power_consumption = gamma_rate * epsilon_rate
 
 #input the numbers from the dataset
 #--------------------------------------------------------------
-lst = open("input.dat").readlines()
+lst = open("input2.dat").readlines()
 
 arr = np.array(lst)
 
-#testing pulling the array elements
-def most_frequent(i, gamma_rate, epsilon_rate):
-    count_bit0 = 0
-    count_bit1 = 0
-    #print(type(i), i)
-    if int(i) == 0:
-        count_bit0 += 1
-    else:
-        count_bit1 += 1
-    if count_bit0 > count_bit1:
-        gamma_rate.append(i)
-        return gamma_rate
-    else:
-        epsilon_rate.append(i)
-        return epsilon_rate
-
 def main():
+    #iterate through each column of the dataset
     for col in zip(*arr):
-        for i in col:
-            print(i)
-            most_frequent(i, gamma_rate, epsilon_rate)
+        #identify the mode (most common) character in the column
+        most_frequent = st.mode(col)
+
+        #add the mode to the gamma_rate list
+        gamma_rate.append(most_frequent)
+
+    #once we have the gamma_rate values, the epsilon_rate is the inverse:
+    for i in gamma_rate:
+        if i == "0":
+            epsilon_rate.append("1")
+        else:
+            epsilon_rate.append("0")
     print('the gamma_rate is: ', gamma_rate)
     print('the epsilon_rate is: ', epsilon_rate)
 
